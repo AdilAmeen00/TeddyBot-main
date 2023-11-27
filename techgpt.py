0f4df805cae4341340379e2a23fae911fe54ed38
Ameen001 <81118905+AdilAmeen00@users.noreply.github.com>
2023-11-27 19:05:04 +0530

    Update techgpt.py

--- techgpt.py
+++ techgpt.py
@@ -206,7 +206,20 @@ def run_model(query, model_app):
     # model1 = SentenceTransformer('paraphrase-MiniLM-L3-v2')
     # embedding1 = model1.encode("this is sentence").tolist()
     # model_app = 'Unstructured Text'
-
+    from openai import OpenAI
+    client = OpenAI()
+    
+    response = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+      ]
+    )
+    print("GPOT ", response['choices'][0]['message']['content'])
+    
     if model_app == 'Thought':
         # docs = db.similarity_search(query)
         # # print("The answer based on Text matching search is \n", docs[0].page_content)
@@ -217,6 +230,7 @@ def run_model(query, model_app):
         print(combined_answer['output'])
         return combined_answer['output']
     elif model_app == 'ChatGPT':
+        from openai import OpenAI
         client = OpenAI()
         os.environ["OPENAI_API_KEY"] = "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"
         response = client.chat.completions.create(
