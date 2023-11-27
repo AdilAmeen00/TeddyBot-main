ea1e8dbdff32773841fe6154f062e4c88821a5b4
Ameen001 <81118905+AdilAmeen00@users.noreply.github.com>
2023-11-27 18:42:51 +0530

    Update techgpt.py

--- techgpt.py
+++ techgpt.py
@@ -217,12 +217,23 @@ def run_model(query, model_app):
         print(combined_answer['output'])
         return combined_answer['output']
     elif model_app == 'ChatGPT':
-        os.environ["OPENAI_API_KEY"] = ""
-        model_name = "gpt-3.5-turbo"
-        llm_open = ChatOpenAI(model_name=model_name)
-        retrieval_chain_open = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type='stuff', retriever=retriever, return_source_documents=True)
-        aa = retrieval_chain_open({"query": query})
-        print("This is aa output", aa)
+        client = OpenAI()
+        os.environ["OPENAI_API_KEY"] = "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"
+        response = client.chat.completions.create(
+          model="gpt-4",
+          response_format={ "type": "json_object" },
+          messages=[
+            {"role": "system", "content": "You are a Expert Injection Mold Design Engineer who answers all the queries in a very technically detailed manner. Your answer is neatly structured in paragraphs"},
+            {"role": "user", "content": "What is Injection Mold design engineer ?"}
+            ]
+         )
+        print("this is open ai response", response.choices[0].message.content)
+        
+        # model_name = "gpt-3.5-turbo"
+        # llm_open = ChatOpenAI(model_name=model_name)
+        # retrieval_chain_open = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type='stuff', retriever=retriever, return_source_documents=True)
+        # aa = retrieval_chain_open({"query": query})
+        # print("This is aa output", aa)
         return aa['result']
 
     elif model_app == 'BedRock':   
